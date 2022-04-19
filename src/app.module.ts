47105145cacd9a51547d0eadcdae3db0e85a15df
Adson Sousa de Oliveira <adson.oliveira@compasso.com.br>
2022-04-19 16:31:59 -0300

    chore: create module, controller and service of employees

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeesModule } from './employees/employees.module';
 import config from '../ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config)],
+  imports: [TypeOrmModule.forRoot(config), EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
