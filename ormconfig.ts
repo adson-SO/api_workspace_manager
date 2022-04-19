898b2c49f8ea3d8c58f7cd0e47f3b28f1a5a0a91
Adson Sousa de Oliveira <adson.oliveira@compasso.com.br>
2022-04-19 15:59:44 -0300

    chore: create database connection and employees create table migration

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOptions";
+
+const config: MysqlConnectionOptions = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '123456',
+    database: 'api_workspace_manager',
+    entities: ['dist/src/**/*.entity.js'],
+    migrations: ['dist/src/database/migrations/*.js'],
+    cli: {
+        migrationsDir: 'src/database/migrations'
+    }
+};
+
+export default config;
---
+++ src/database/migrations/1650391639811-create-employee.ts
@@ -0,0 +1,51 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createEmployee1650391639811 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'employees',
+                columns: [
+                    {
+                        name: 'employee_id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'cpf',
+                        type: 'varchar',
+                        isUnique: true,
+                        isNullable: false
+                    },
+                    {
+                        name: 'office',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'birthday',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'situation',
+                        type: 'varchar',
+                        isNullable: false
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('employees');
+    }
+
+}
