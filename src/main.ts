c60a3373b02a6e05afcc76550c7239adb940b684
Adson Sousa de Oliveira <adson.oliveira@compasso.com.br>
2022-04-26 09:24:44 -0300

    docs: generate swagger for the api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+  .setTitle('API Workspace Manager')
+  .setDescription('API REST para gerenciar um negócio, permitindo cadastro de funcionários e produtos.')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/v1/api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
